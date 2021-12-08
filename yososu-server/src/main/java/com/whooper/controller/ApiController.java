d0caf3aa3aed2c7b4dc9a053b46719e1926c3c1e
daeunLee <eleeje97@gmail.com>
2021-12-08 23:51:23 +0900

    add 'searchByAddr' api

--- yososu-server/src/main/java/com/whooper/controller/ApiController.java
+++ yososu-server/src/main/java/com/whooper/controller/ApiController.java
@@ -26,7 +26,43 @@ public class ApiController {
         urlBuilder.append("&" + URLEncoder.encode("perPage", "UTF-8") + "=365");
         urlBuilder.append("&" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey);
 
-        URL url = new URL(urlBuilder.toString());
+        YososuResponse  response = callApi(urlBuilder.toString());
+
+        return response;
+    }
+
+
+
+    // 주소로 검색
+    @GetMapping("/searchByAddr")
+    public @ResponseBody YososuResponse searchByAddr(@RequestParam String addr) throws IOException {
+
+        StringBuilder urlBuilder = new StringBuilder(baseUrl + "/uws/v1/inventory");
+        urlBuilder.append("?" + URLEncoder.encode("page", "UTF-8") + "=1");
+        urlBuilder.append("&" + URLEncoder.encode("perPage", "UTF-8") + "=365");
+        urlBuilder.append("&" + URLEncoder.encode("cond[addr::LIKE]", "UTF-8") + "=" + addr);
+        urlBuilder.append("&" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey);
+
+
+        YososuResponse response = callApi(urlBuilder.toString());
+
+        return response;
+    }
+
+
+
+    // 모든 주유소 가격으로 정렬
+
+    // 모든 주유소 재고량으로 정렬
+
+    // 주소검색결과 가격으로 정렬
+
+    // 주소검색결과 재고량으로 정렬
+
+
+
+    public YososuResponse callApi(String url_str) throws IOException {
+        URL url = new URL(url_str);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
@@ -35,9 +71,9 @@ public class ApiController {
         System.out.println("Response code: " + conn.getResponseCode());
         BufferedReader rd;
         if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
         } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(),"UTF-8"));
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
         }
         StringBuilder sb = new StringBuilder();
         String line;
@@ -56,19 +92,5 @@ public class ApiController {
 
         return response;
     }
-
-
-
-    // 주소로 검색
-
-
-    // 모든 주유소 가격으로 정렬
-
-    // 모든 주유소 재고량으로 정렬
-
-    // 주소검색결과 가격으로 정렬
-
-    // 주소검색결과 재고량으로 정렬
-
 
 }
